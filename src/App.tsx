8fe9a43747ffd14875f7d10811ced67aa8928979
Raphael Jans S. Caballegan <raphael.caballegan@gmail.com>
2024-03-20 19:30:04 +0800

    added transformers.js

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 
 function App() {
 	return (
-		<div className="w-screen h-screen justify-center items-center">
+		<div className="w-screen h-screen flex justify-center items-center">
 			<h1 className="text-3xl font-bold">Vite + React</h1>
 		</div>
 
